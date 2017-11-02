6f825c8bb0e8412c9f20adf655740bfa3652db7e
Brian McCann <Brian@Users-MBP.home>
2017-11-01 20:54:45 -0400

    made changes and committing branch2

--- server/db/index.js
+++ server/db/index.js
@@ -4,3 +4,16 @@ const db = require('./db')
 require('./models')
 
 module.exports = db
+
+function before(you){
+  const puppy = 'pupy'
+  const a = 'b'
+  return 'some more logic...'
+}
+
+
+
+
+
+
+// a comment for good measure !
